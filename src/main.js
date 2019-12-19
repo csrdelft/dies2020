469a0139c8112904af64d5cf5479883a9e3a0185
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-12-19 15:15:16 +0100

    Optimizeer titel, maak css kleiner

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
 import './assets/style.scss'
-
-import YouTubePlayer from "youtube-player";
-
-YouTubePlayer('player', {playerVars: {controls: 0, disablekb: 1, showinfo: 0, rel: 0, autohide: 1,}})
-    .loadVideoById('lA7n7TwPDmw');
-
