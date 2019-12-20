8cb07f659e30978e7b949680af27e0f60b59d547
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-12-20 11:29:50 +0100

    Laad video na het laden van de pagina

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import './assets/style.scss'
+
+window.addEventListener('load', function () {
+    document.getElementById("player").innerHTML =
+        "<iframe class=\"embed-responsive-item\" src=\"https://drive.google.com/file/d/1EwuYw0WCsDI8Au65INKAP_uAKi9CocLT/preview\"></iframe>"
+});
